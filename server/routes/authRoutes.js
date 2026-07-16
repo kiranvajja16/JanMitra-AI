23a249b7fd6aa5a1cc87a21a725dd92956730c9e
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-16 09:49:45 +0530

    JWT middleware is created

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,18 @@
 const express= require('express');
 
 const {registerUser,loginUser}=require('../controllers/authController');
+const protect = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.get('/profile',protect,(req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to your profile!',
+        user:req.user,
+    });
+});
 
 module.exports=router;
