3352d54d5a15280769c94bd69a92b35683f23e3f
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-19 14:12:45 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 const app = require("./app");
@@ -6,10 +5,8 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
-
 connectDB();
 
-
-app.listen(PORT, "127.0.0.1", () => {
-    console.log(` Server is running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
